bc1ba220637c593606e74a7bf3035963ce99c205
Milton <miltonbordi@gmail.com>
2021-12-08 10:06:11 -0300

    Homework JS2 done

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -191,11 +191,11 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-let sasa=[]
+let seisTabla=[]
 for(var i=0; i<11; i++) {
-  sasa.push(6*i);
+  seisTabla.push(6*i);
 }
-return sasa;
+return seisTabla;
 }
 
 function tieneTresDigitos(numero){
